17f12e9c1f01a0d1f04695a35955008fddb507e9
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2021-04-11 17:16:59 -0700

    Add project files.

---
+++ MazeBot/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MazeBot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //this is the page that would display the maze
+        private mazeGenerator mazePage;
+        private WebPageHref webPage;
+        private developersPage devPage;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.mazePage = new mazeGenerator();
+            this.webPage = new WebPageHref();
+            this.devPage = new developersPage();
+        }
+
+
+        /*  The function is called when clicking on Maze button
+         *  Will display the mazeGenerator page created after InitializateComponents
+         */
+        private void MazeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainPanel.Content = this.mazePage;
+        }
+
+        private void WebButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainPanel.Content = this.webPage;
+        }
+
+        private void DevelopersButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainPanel.Content = this.devPage;
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MazeBot/mazeGenerator.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MazeBot
+{
+    /// <summary>
+    /// Interaction logic for mazeGenerator.xaml
+    /// </summary>
+    public partial class mazeGenerator : Page
+    {
+        private int rows;
+        private int columns;
+        private int nodeSize;
+
+        public mazeGenerator()
+        {
+            InitializeComponent();
+        }
+
+        /*      GENERATE MAZE BUTTON
+         *      When button is clicked
+         *      catch from the text:
+         *          rows
+         *          columns
+         *          nodesize
+         *      if failed default val:
+         *          rws = 20
+         *          col = 20
+         *          ndsz = 10
+         */
+        private void GenrateMazeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.rows = catchRows();
+            this.columns = catchColumns();
+            this.nodeSize = catchNodeSize();
+            output_text();
+        }
+
+        /*
+         *      OUT PUT DATA ABOUT MAZE
+         *      this function simply outputs data in the databox x:Name = output
+         */
+        private void output_text()
+        {
+            output.Text = "Rows: " + this.rows +
+                "\nColumns: " + this.columns +
+                "\nNode Size: " + this.nodeSize;
+        }
+
+        /*
+         *      CATCHING ROWS NUMBER
+         */
+        private int catchRows()
+        {
+            int num;
+
+            try
+            {
+                num = Int32.Parse(rows_textBox.Text);
+            }
+            catch
+            {
+                num = 20;
+            }
+
+            return num;
+        }
+
+        /*
+         *      CATCHING # OF COLUMNS
+         */
+        private int catchColumns()
+        {
+            int num;
+
+            try
+            {
+                num = Int32.Parse(columns_textBox.Text);
+            }
+            catch
+            {
+                num = 20;
+            }
+
+            return num;
+        }
+
+        /*
+         *      CATCHING NODE SIZE
+         */
+        private int catchNodeSize()
+        {
+            int num;
+
+            try
+            {
+                num = Int32.Parse(nodeSize_textBox.Text);
+            }
+            catch
+            {
+                num = 10;
+            }
+
+            return num;
+        }
+    }
+}
